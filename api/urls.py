60b2584f4eb26778c0bb130d56d6a1cf23829770
Abzed Abdul Mohammed <ibraabzed@gmail.com>
2022-04-12 23:26:40 +0300

    add jwt to requirements

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 urlpatterns=[
@@ -18,4 +19,5 @@ urlpatterns=[
     path('api/token-auth', obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
