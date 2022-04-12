82661caf83f952dd5a0ec5afc4db8b28216e472f
Abzed Abdul Mohammed <ibraabzed@gmail.com>
2022-04-13 01:10:34 +0300

    removed token refresh

--- api/urls.py
+++ api/urls.py
@@ -18,6 +18,6 @@ urlpatterns=[
     path('classrooms/<int:pk>', ClassroomData.as_view()),
     path('api/token-auth', obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
